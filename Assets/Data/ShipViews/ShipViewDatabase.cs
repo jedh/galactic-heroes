59cb4661381784b41e6ab396de021e62b55c293f
Jed <jedh@outlook.com>
2019-11-20 21:47:17 -0400

    Added  database scriptable objects to store other ship scriptable objects.

---
+++ Assets/Data/ShipViews/ShipViewDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GH.Data
+{
+    [CreateAssetMenu(fileName = "ship_view_database", menuName = "GH/Ship View Database")]
+    public class ShipViewDatabase : ScriptableObject
+    {
+        public List<ShipViewData> ShipViews;
+    }
+}
