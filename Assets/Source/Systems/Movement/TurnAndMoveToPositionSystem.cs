3e30aeff8842c66dcb397f3ebfefd3a91e666471
stephenabertrand <stephen.a.bertrand@gmail.com>
2019-11-24 00:34:51 -0400

    Implemented TurnAndMoveToPositionSystem.

---
+++ Assets/Source/Systems/Movement/TurnAndMoveToPositionSystem.cs
@@ -0,0 +1,24 @@
+﻿using GH.Components;
+using GH.SystemGroups;
+using Unity.Entities;
+
+namespace GH.Systems
+{
+    [UpdateInGroup(typeof(BattleLogicSystemGroup))]
+    public class TurnAndMoveToPositionSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            Entities.WithAll<TurnAndMove>().ForEach((Entity entity, ref MoveToPosition moveTo) =>
+            {
+                PostUpdateCommands.RemoveComponent<MoveToPosition>(entity);
+                
+                PostUpdateCommands.AddComponent<TranslateToPosition>(entity);
+                PostUpdateCommands.SetComponent(entity, new TranslateToPosition() { Value = moveTo.Value });  // start translating.
+
+                PostUpdateCommands.AddComponent<RotateTowardsPosition>(entity);
+                PostUpdateCommands.SetComponent(entity, new RotateTowardsPosition() { Value = moveTo.Value });  // start rotating.
+            });
+        }
+    }
+}
