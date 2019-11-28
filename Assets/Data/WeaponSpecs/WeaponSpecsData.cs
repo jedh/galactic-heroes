d553ef56f19ac98d7f3dfb77e988a8647a705ce2
jed <jedh@outlook.com>
2019-11-27 23:49:34 -0400

    Updating the ai and weapons schema.

--- Assets/Data/WeaponSpecs/WeaponSpecsData.cs
+++ Assets/Data/WeaponSpecs/WeaponSpecsData.cs
@@ -7,6 +7,33 @@ namespace GH.Data
     [CreateAssetMenu(fileName = "weapon_specs", menuName = "GH/Weapon Specs")]
     public class WeaponSpecsData : ScriptableObject
     {
-        
+        public int ID;
+
+        public string NameID;
+
+        public float Damage;
+
+        public float ShieldDamageModifier;
+
+        public float HullDamageModifer;
+
+        public float MinRange;
+
+        public float MaxRange;
+
+        public float ChargeTime;
+
+        public float ProjectileSpeed;
+
+        public int SalvoCount;        
+
+        public float SplashRadius;
+
+        public float Lifetime;
+
+        private void Awake()
+        {
+            ID = GetHashCode();
+        }
     }
 }
