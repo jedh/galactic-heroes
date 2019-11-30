6267848fdaed168503d2e5f9daa4f0980a2d53db
stephenabertrand <stephen.a.bertrand@gmail.com>
2019-11-30 00:54:15 -0400

    DeployToPositionSystem now handles linear and angular velocity calculations in one query.

--- Assets/Source/Systems/Movement/DeployToPositionSystem.cs
+++ Assets/Source/Systems/Movement/DeployToPositionSystem.cs
@@ -7,135 +7,114 @@ using UnityEngine;
 
 namespace GH.Systems
 {
-    [UpdateInGroup(typeof(BattleLogicSystemGroup))]
+    [UpdateInGroup(typeof(BattleLogicSystemGroup)), UpdateBefore(typeof(TranslationSystem)), UpdateBefore(typeof(RotationSystem))]  // updates before so that deltaTime calculations are correct.
     public class DeployToPositionSystem : ComponentSystem
     {
-        private const float k_DistanceFromTarget = 0.02f;
+        private const float k_DistanceTolerance = 0.02f;
+        private const float k_AngleTolerance = 0.9999f;
 
         protected override void OnUpdate()
         {
-            Entities.ForEach((Entity entity, ref MovementStats stats, ref Translation translation, ref Velocity velocity, ref AngularVelocity angularVelocity, ref Rotation rotation, ref DeployToPosition moveToPosition) =>
+            Entities.ForEach(
+                (Entity entity,
+                ref DeployToPosition deployment, ref MovementStats stats, ref Translation translation, ref Rotation rotation, ref Velocity velocity, ref AngularVelocity angularVelocity) =>
             {
-                PostUpdateCommands.RemoveComponent<RotateTowardsPosition>(entity);
-                PostUpdateCommands.RemoveComponent<MoveForward>(entity);
-                angularVelocity.Velocity = 0f;
+                float3 toTarget = deployment.Position - translation.Value;
+                float distance = math.length(toTarget);
 
-                /* if facing close enough
-                    update ang vel
-                    add move forward
-                    accel/decel
-                else
-                    if speed is slow enough
-                         add ang vel
-                    decel
-                */
-
-                float speed = math.length(velocity.Value);
-                if(speed > stats.MaxSpeedToTurn)
+                if (deployment.ShouldStop && distance <= k_DistanceTolerance)
                 {
-                    if (speed != 0f)
-                    {
-                        speed = math.max(speed - stats.Deceleration * Time.deltaTime * 0.5f, 0f);
+                    velocity.Value = float3.zero;
+                    angularVelocity.Velocity = 0f;
+                    translation.Value = deployment.Position;
 
-                        velocity.Value = math.normalize(velocity.Value) * speed;
-                    }
-                }
-                else
-                {
                     PostUpdateCommands.RemoveComponent<DeployToPosition>(entity);
 
-                    PostUpdateCommands.AddComponent<DeployingTo>(entity);
-                    PostUpdateCommands.SetComponent(entity, new DeployingTo() { Position = moveToPosition.Position, ShouldStop = moveToPosition.ShouldStop });
-
-                    PostUpdateCommands.AddComponent<FaceTarget>(entity);
-                    PostUpdateCommands.SetComponent(entity, new FaceTarget() { Value = moveToPosition.Position });
+                    return; // we're there, stop.
                 }
-            });
 
-            Entities.WithNone<MoveForward>().ForEach((Entity entity, ref MovementStats stats, ref Translation translation, ref Velocity velocity, ref Rotation rotation, ref DeployingTo target) =>
-            {
-                float3 toPos = math.normalize(target.Position - translation.Value);
+
+                float speed = math.length(velocity.Value);
+
                 float3 forward = math.normalize(math.forward(rotation.Value));
+                float3 velocityDirection = speed == 0f ? forward : math.normalize(velocity.Value);
 
-                float costheta = math.dot(toPos, forward);
+                toTarget = toTarget / distance; // normalize
+                float costheta = math.dot(toTarget, forward);
 
-                if (costheta >= stats.ThrustTolerance) // have we turned enough to begin moving forward?
+
+
+                // Rotation
                 {
-                    float currentForwardSpeed = math.dot(forward, velocity.Value);
-                    float forwardSpeed = math.min(currentForwardSpeed + stats.Acceleration * Time.deltaTime * Time.deltaTime, stats.TopSpeed);
+                    float3 rotationAxis = costheta == -1 || costheta == 1 ? math.up() : math.normalize(math.cross(forward, toTarget));
+                    float rotationDirection = math.saturate(math.sign(math.dot(rotationAxis, math.up())) + 1) * 2 - 1;  // -1 or 1
+                    float rotationSpeed = math.radians(stats.RotationSpeed) * rotationDirection;
+                    rotationAxis = rotationAxis * rotationDirection;
 
-                    PostUpdateCommands.AddComponent<MoveForward>(entity);
-                    PostUpdateCommands.SetComponent(entity, new MoveForward() { Speed = forwardSpeed });
-                }
-                else
-                {
-                    float speed = math.length(velocity.Value);
-
-                    if (speed != 0f)
+                    if (costheta >= stats.ThrustTolerance)  // have we turned enough to begin moving forward?
                     {
-                        speed = math.max(speed - stats.Deceleration * Time.deltaTime * 0.5f, 0f);
-
-                        velocity.Value = math.normalize(velocity.Value) * speed;
-                    }
-                }
-            });
-
-            Entities.ForEach((Entity entity, ref MovementStats stats, ref Translation translation, ref Velocity velocity, ref Rotation rotation, ref DeployingTo moving, ref MoveForward moveForward) =>
-            {
-                float3 toTarget = moving.Position - translation.Value;
-
-                float t = Time.deltaTime;
-                float a = stats.Acceleration;
-                float d = stats.Deceleration;
-                float speed = moveForward.Speed;
-                float distance = math.length(toTarget);
-                float3 forward = math.normalize(math.forward(rotation.Value));
-
-                if (moving.ShouldStop)
-                {
-                    if (distance <= k_DistanceFromTarget)
-                    {
-                        moveForward.Speed = 0f;
-
-                        velocity.Value = float3.zero;
-                        translation.Value = moving.Position;
-
-                        PostUpdateCommands.RemoveComponent<DeployingTo>(entity);
-                        PostUpdateCommands.RemoveComponent<FaceTarget>(entity);
-                        PostUpdateCommands.RemoveComponent<MoveForward>(entity);
-
-                        return; // we're there, stop.
-                    }
-
-                    float speedInDirection = math.dot(math.normalize(toTarget), forward * speed);
-
-                    // https://math.stackexchange.com/questions/233107/finding-minimum-distance-traveled-with-specified-deceleration-from-starting-spee
-                    float distanceTravelledIfDecelerating = (speedInDirection * speedInDirection) / (2 * d);
-
-                    if (distance <= distanceTravelledIfDecelerating)
-                    {
-                        speed = math.min(speed - d * t * 0.5f, stats.TopSpeed);
+                        velocityDirection = forward;
                     }
                     else
                     {
-                        speed = math.min(speed + a * t * 0.5f, stats.TopSpeed);
+                        rotationSpeed *= math.step(speed, /* < */ stats.MaxSpeedToTurn);    // only turn if we've slowed down enough
                     }
 
-                    speedInDirection = math.dot(math.normalize(toTarget), forward * speed);
-
-                    float expectedDistanceThisFrame = distance - speedInDirection * t;
-                    if (expectedDistanceThisFrame < k_DistanceFromTarget)
+                    if (costheta >= k_AngleTolerance)
                     {
-                        Debug.LogWarning("Stopping");
-                        speed = distance / Time.deltaTime; // take us right there this frame.
+                        float angleRadians = math.acos(costheta);
+                        rotationSpeed = (float.IsNaN(angleRadians) ? 0f : angleRadians) * rotationDirection / Time.deltaTime;    // take us directly there this frame.
                     }
-                }
-                else
-                {
-                    speed = math.min(speed + a * t * 0.5f, stats.TopSpeed);
+
+                    velocityDirection = math.rotate(quaternion.AxisAngle(rotationAxis, rotationSpeed * Time.deltaTime), velocityDirection);
+
+                    angularVelocity.Axis = rotationAxis;
+                    angularVelocity.Velocity = rotationSpeed;
                 }
 
-                moveForward.Speed = speed;
+
+
+                // Translation
+                {
+                    if (costheta >= stats.ThrustTolerance)  // have we turned enough to begin moving forward?
+                    {
+                        if (deployment.ShouldStop)
+                        {
+                            // https://math.stackexchange.com/questions/233107/finding-minimum-distance-traveled-with-specified-deceleration-from-starting-spee
+                            float speedInDirection = math.dot(toTarget, velocityDirection * speed);
+                            float distanceTravelledIfDecelerating = (speedInDirection * speedInDirection) / (2 * stats.Deceleration);
+
+                            if (distanceTravelledIfDecelerating >= distance)
+                            {
+                                speed = math.clamp(speed - 0.5f * stats.Deceleration * Time.deltaTime, 0f, stats.TopSpeed);   // t(v - at/2)
+                            }
+                            else
+                            {
+                                speed = math.clamp(speed + 0.5f * stats.Acceleration * Time.deltaTime, 0f, stats.TopSpeed);   // t(v - at/2)
+                            }
+
+                            speedInDirection = math.dot(toTarget, velocityDirection * speed);
+
+                            float distanceThisFrame = distance - speedInDirection * Time.deltaTime;
+                            if (distanceThisFrame < k_DistanceTolerance)
+                            {
+                                speed = distance / Time.deltaTime; // take us right there this frame.
+                            }
+                        }
+                        else
+                        {
+                            // don't stop, just keep accelerating.
+                            speed = math.clamp(speed + 0.5f * stats.Acceleration * Time.deltaTime, 0f, stats.TopSpeed);   // t(v - at/2)
+                        }
+                    }
+                    else
+                    {
+                        // decelerate
+                        speed = math.clamp(speed - 0.5f * stats.Deceleration * Time.deltaTime, 0f, stats.TopSpeed);   // t(v - at/2)
+                    }
+
+                    velocity.Value = velocityDirection * speed;
+                }
             });
         }
     }
