640f96900a65e909d094afcbfe5b4f6d308ce071
stephen.bertrand <stephen.bertrand@ubisoft.com>
2019-12-06 15:21:22 -0400

    stubbed out some basic weapons system code

---
+++ Assets/Source/Systems/Weapons/BasicWeaponsSystem.cs
@@ -0,0 +1,62 @@
+﻿using GH.Components;
+using GH.SystemGroups;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace GH.Systems
+{
+    [UpdateInGroup(typeof(BattleLogicSystemGroup))]
+    public class BasicWeaponsSystem : JobComponentSystem
+    {
+        private EntityCommandBufferSystem m_EntityCommandBufferSystem;
+
+        protected override void OnCreate()
+        {
+            m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+            base.OnCreate();
+        }
+
+        protected override void OnDestroy()
+        {
+            m_EntityCommandBufferSystem = null;
+            base.OnDestroy();
+        }
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var commandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+
+            FireWeaponJob fireWeaponJob = new FireWeaponJob() { CommandBuffer = commandBuffer };
+
+            var beginJobHandle = fireWeaponJob.Schedule(this, inputDeps);
+
+            m_EntityCommandBufferSystem.AddJobHandleForProducer(beginJobHandle);
+
+            return beginJobHandle;
+        }
+
+        //--------------------------
+        // Jobs
+        //--------------------------
+
+        [ExcludeComponent(typeof(DeployToPosition))]
+        struct FireWeaponJob : IJobForEachWithEntity<Target>
+        {
+            public EntityCommandBuffer.Concurrent CommandBuffer;
+
+            public void Execute(Entity entity, int jobIndex, [ReadOnly] ref Target target)
+            {
+                // determine if we should fire
+                    // Check distance to target
+                    // firing arc
+                    // friendly fire?
+
+                // create flag to fire weapons
+            }
+        }
+    }  
+ }
