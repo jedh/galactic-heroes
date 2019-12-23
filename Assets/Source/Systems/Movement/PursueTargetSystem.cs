e049405b6cbe0407f2a7e37af4c2d1a22658f60a
jedh <jed.hubic@ubisoft.com>
2019-12-23 14:12:15 -0400

    Updated pursuit to use relative distance to scale prediction.

--- Assets/Source/Systems/Movement/PursueTargetSystem.cs
+++ Assets/Source/Systems/Movement/PursueTargetSystem.cs
@@ -12,152 +12,164 @@ using UnityEngine;
 
 namespace GH.Systems
 {
-    [UpdateInGroup(typeof(BattleLogicSystemGroup))]
-    public class PursueTargetSystem : JobComponentSystem
-    {
-        private EntityCommandBufferSystem m_EntityCommandBufferSystem;
+	[UpdateInGroup(typeof(BattleLogicSystemGroup))]
+	public class PursueTargetSystem : JobComponentSystem
+	{
+		private EntityCommandBufferSystem m_EntityCommandBufferSystem;
 
-        // Increase/decrease this value for different pursuit effectiveness.
-        private float m_LookAheadTimeStep = .5f;
+		// Increase/decrease this value for different pursuit effectiveness.
+		private float m_LookAheadTimeStep = 3f;
 
-        protected override void OnCreate()
-        {
-            m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-            base.OnCreate();
-        }
+		protected override void OnCreate()
+		{
+			m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+			base.OnCreate();
+		}
 
-        protected override void OnDestroy()
-        {
-            m_EntityCommandBufferSystem = null;
-            base.OnDestroy();
-        }
+		protected override void OnDestroy()
+		{
+			m_EntityCommandBufferSystem = null;
+			base.OnDestroy();
+		}
 
-        protected override JobHandle OnUpdate(JobHandle inputDeps)
-        {
-            var commandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
-            var translationData = GetComponentDataFromEntity<Translation>(true);
-            var velocityData = GetComponentDataFromEntity<Velocity>(true);
+		protected override JobHandle OnUpdate(JobHandle inputDeps)
+		{
+			var commandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+			var translationData = GetComponentDataFromEntity<Translation>(true);
+			var velocityData = GetComponentDataFromEntity<Velocity>(true);
 
-            BeginPursuitJob beginDeploymentJob = new BeginPursuitJob()
-            {
-                TranslationData = translationData,
-                VelocityData = velocityData,
-                LookAheadTimeStep = m_LookAheadTimeStep,
-                CommandBuffer = commandBuffer
-            };
+			BeginPursuitJob beginDeploymentJob = new BeginPursuitJob()
+			{
+				TranslationData = translationData,
+				VelocityData = velocityData,
+				LookAheadTimeStep = m_LookAheadTimeStep,
+				CommandBuffer = commandBuffer,
+				DeltaTime = Time.deltaTime
+			};
 
-            UpdatePursuitJob updateDeploymentJob = new UpdatePursuitJob()
-            {
-                TranslationData = translationData,
-                VelocityData = velocityData,
-                LookAheadTimeStep = m_LookAheadTimeStep
-            };
+			UpdatePursuitJob updateDeploymentJob = new UpdatePursuitJob()
+			{
+				TranslationData = translationData,
+				VelocityData = velocityData,
+				LookAheadTimeStep = m_LookAheadTimeStep,
+				DeltaTime = Time.deltaTime
+			};
 
-            PursuitJobFinished deploymentFinishedJob = new PursuitJobFinished() { CommandBuffer = commandBuffer };
+			PursuitJobFinished deploymentFinishedJob = new PursuitJobFinished() { CommandBuffer = commandBuffer };
 
-            var updateJobHandle = updateDeploymentJob.Schedule(this, inputDeps);
-            var beginJobHandle = beginDeploymentJob.Schedule(this, updateJobHandle);
-            var finishedJobHandle = deploymentFinishedJob.Schedule(this, beginJobHandle);
+			var updateJobHandle = updateDeploymentJob.Schedule(this, inputDeps);
+			var beginJobHandle = beginDeploymentJob.Schedule(this, updateJobHandle);
+			var finishedJobHandle = deploymentFinishedJob.Schedule(this, beginJobHandle);
 
-            m_EntityCommandBufferSystem.AddJobHandleForProducer(finishedJobHandle);
+			m_EntityCommandBufferSystem.AddJobHandleForProducer(finishedJobHandle);
 
-            return finishedJobHandle;
-        }
+			return finishedJobHandle;
+		}
 
-        //////////
-        // JOBS //
-        //////////
+		//////////
+		// JOBS //
+		//////////
 
-        // Add pursuit movment if it doesn't exist.
-        [ExcludeComponent(typeof(PursueTarget), typeof(CombatMovement))]
-        struct BeginPursuitJob : IJobForEachWithEntity<Target, WeaponStats, MovementStats, DeployToPosition>
-        {
-            [ReadOnly]
-            public ComponentDataFromEntity<Translation> TranslationData;
+		// Add pursuit movment if it doesn't exist.
+		[ExcludeComponent(typeof(PursueTarget), typeof(CombatMovement))]
+		struct BeginPursuitJob : IJobForEachWithEntity<Target, WeaponStats, MovementStats, DeployToPosition>
+		{
+			[ReadOnly]
+			public ComponentDataFromEntity<Translation> TranslationData;
 
-            [ReadOnly]
-            public ComponentDataFromEntity<Velocity> VelocityData;
+			[ReadOnly]
+			public ComponentDataFromEntity<Velocity> VelocityData;
 
-            public EntityCommandBuffer.Concurrent CommandBuffer;
+			public EntityCommandBuffer.Concurrent CommandBuffer;
 
-            [ReadOnly]
-            public float LookAheadTimeStep;
+			[ReadOnly]
+			public float LookAheadTimeStep;
 
-            public void Execute(Entity entity, int index, [ReadOnly] ref Target target, [ReadOnly] ref WeaponStats weaponStats, [ReadOnly] ref MovementStats movementStats, ref DeployToPosition deployToPosition)
-            {
-                var targetPosition = TranslationData[target.TargetEntity];
-                var targetVelocity = VelocityData[target.TargetEntity];
-                var desiredPosition = targetPosition.Value + targetVelocity.Value * LookAheadTimeStep;
-                deployToPosition.Position = desiredPosition;
-                deployToPosition.ShouldStop = !movementStats.DoesSwarm;
-                //CommandBuffer.SetComponent(jobIndex, entity, new DeployToPosition() { Position = position.Value, ShouldStop = !movementStats.DoesSwarm });
-                CommandBuffer.AddComponent(index, entity, new CombatMovement()
-                {
-                    MinRangeSq = weaponStats.MinRange * weaponStats.MinRange,
-                    MaxRangeSq = weaponStats.MaxRange * weaponStats.MaxRange,
-                    OptimalRangeSq = weaponStats.OptimalRange * weaponStats.OptimalRange
-                });
+			[ReadOnly]
+			public float DeltaTime;
 
-                CommandBuffer.AddComponent(index, entity, default(PursueTarget));
-            }
-        }
+			public void Execute(Entity entity, int index, [ReadOnly] ref Target target, [ReadOnly] ref WeaponStats weaponStats, [ReadOnly] ref MovementStats movementStats, ref DeployToPosition deployToPosition)
+			{
+				var targetPosition = TranslationData[target.TargetEntity];
+				var targetVelocity = VelocityData[target.TargetEntity];
+				var desiredPosition = targetPosition.Value + targetVelocity.Value * LookAheadTimeStep * DeltaTime;
+				deployToPosition.Position = desiredPosition;
+				deployToPosition.ShouldStop = !movementStats.DoesSwarm;
+				//CommandBuffer.SetComponent(jobIndex, entity, new DeployToPosition() { Position = position.Value, ShouldStop = !movementStats.DoesSwarm });
+				CommandBuffer.AddComponent(index, entity, new CombatMovement()
+				{
+					MinRangeSq = weaponStats.MinRange * weaponStats.MinRange,
+					MaxRangeSq = weaponStats.MaxRange * weaponStats.MaxRange,
+					OptimalRangeSq = weaponStats.OptimalRange * weaponStats.OptimalRange
+				});
 
-        // Update pursuit targets based on whether ships swarm or not.       
-        struct UpdatePursuitJob : IJobForEachWithEntity<Target, DeployToPosition, Translation, CombatMovement, PursueTarget>
-        {
-            [ReadOnly]
-            public ComponentDataFromEntity<Translation> TranslationData;
+				CommandBuffer.AddComponent(index, entity, default(PursueTarget));
+			}
+		}
 
-            [ReadOnly]
-            public ComponentDataFromEntity<Velocity> VelocityData;
+		// Update pursuit targets based on whether ships swarm or not.       
+		struct UpdatePursuitJob : IJobForEachWithEntity<Target, DeployToPosition, Translation, CombatMovement, PursueTarget, MovementStats>
+		{
+			[ReadOnly]
+			public ComponentDataFromEntity<Translation> TranslationData;
 
-            [ReadOnly]
-            public float LookAheadTimeStep;
+			[ReadOnly]
+			public ComponentDataFromEntity<Velocity> VelocityData;
 
-            public void Execute(Entity entity, int index, [ReadOnly] ref Target target, ref DeployToPosition deployToPosition, [ReadOnly] ref Translation translation, [ReadOnly] ref CombatMovement combatMovement, [ReadOnly] ref PursueTarget pursueTarget)
-            {
-                if (TranslationData.Exists(target.TargetEntity) &&
-                    VelocityData.Exists(target.TargetEntity))
-                {
-                    var targetPosition = TranslationData[target.TargetEntity];
-                    var targetVelocity = VelocityData[target.TargetEntity];
-                    var distanceSq = math.distancesq(targetPosition.Value, translation.Value);
-                    if (distanceSq > combatMovement.OptimalRangeSq)
-                    {
-                        if (combatMovement.OptimalRangeSq != 0f)
-                        {
-                            var direction = targetPosition.Value - translation.Value;
-                            direction = math.normalizesafe(direction);
-                            var optimalPosition = translation.Value + (direction * math.sqrt(combatMovement.OptimalRangeSq));
+			[ReadOnly]
+			public float LookAheadTimeStep;
 
-                            NaNDebugger.IsNan(targetPosition.Value.x, "target nan");
+			[ReadOnly]
+			public float DeltaTime;
 
-                            deployToPosition.Position = optimalPosition;
-                        }
-                        else
-                        {
-                            var desiredPosition = targetPosition.Value + targetVelocity.Value * LookAheadTimeStep;
-                            deployToPosition.Position = desiredPosition;
-                        }
-                    }
-                    else
-                    {
-                        deployToPosition.Position = translation.Value;
-                    }
-                }
-            }
-        }
+			public void Execute(Entity entity, int index, [ReadOnly] ref Target target, ref DeployToPosition deployToPosition, [ReadOnly] ref Translation translation, [ReadOnly] ref CombatMovement combatMovement, [ReadOnly] ref PursueTarget pursueTarget, [ReadOnly] ref MovementStats movementStats)
+			{
+				if (TranslationData.Exists(target.TargetEntity) &&
+					VelocityData.Exists(target.TargetEntity))
+				{
+					var targetPosition = TranslationData[target.TargetEntity];
+					var targetVelocity = VelocityData[target.TargetEntity];
+					var distanceSq = math.distancesq(targetPosition.Value, translation.Value);
+					if (distanceSq > combatMovement.OptimalRangeSq)
+					{
+						if (combatMovement.OptimalRangeSq != 0f)
+						{
+							var direction = targetPosition.Value - translation.Value;
+							direction = math.normalizesafe(direction);
+							var optimalPosition = translation.Value + (direction * math.sqrt(combatMovement.OptimalRangeSq));
 
-        [ExcludeComponent(typeof(DeployToPosition))]
-        struct PursuitJobFinished : IJobForEachWithEntity<CombatMovement, PursueTarget>
-        {
-            public EntityCommandBuffer.Concurrent CommandBuffer;
+							NaNDebugger.IsNan(targetPosition.Value.x, "target nan");
 
-            public void Execute(Entity entity, int index, [ReadOnly] ref CombatMovement combatMovement, [ReadOnly] ref PursueTarget pursueTarget)
-            {
-                CommandBuffer.RemoveComponent<CombatMovement>(index, entity);
-                CommandBuffer.RemoveComponent<PursueTarget>(index, entity);
-            }
-        }
-    }
+							deployToPosition.Position = optimalPosition;
+						}
+						else
+						{
+							// This version ignores the timestep and uses relative distance instead.
+							var t = math.length(math.sqrt(distanceSq)) * movementStats.TopSpeed;
+							var desiredPosition = targetPosition.Value + targetVelocity.Value * t * DeltaTime;
+
+							//var desiredPosition = targetPosition.Value + targetVelocity.Value * LookAheadTimeStep * DeltaTime;
+							deployToPosition.Position = desiredPosition;
+						}
+					}
+					else
+					{
+						deployToPosition.Position = translation.Value;
+					}
+				}
+			}
+		}
+
+		[ExcludeComponent(typeof(DeployToPosition))]
+		struct PursuitJobFinished : IJobForEachWithEntity<CombatMovement, PursueTarget>
+		{
+			public EntityCommandBuffer.Concurrent CommandBuffer;
+
+			public void Execute(Entity entity, int index, [ReadOnly] ref CombatMovement combatMovement, [ReadOnly] ref PursueTarget pursueTarget)
+			{
+				CommandBuffer.RemoveComponent<CombatMovement>(index, entity);
+				CommandBuffer.RemoveComponent<PursueTarget>(index, entity);
+			}
+		}
+	}
 }
